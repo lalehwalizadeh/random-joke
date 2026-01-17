dc90674dcd4c16ef2cfe4229679ecb1e929e4dc7
LalehWalizadeh <lalah.walizadah2022@gmail.com>
2026-01-17 17:40:32 +0430

    color changing

--- script.js
+++ script.js
@@ -19,5 +19,24 @@ const generateJoke = () => {
 	xhr.send();
 };
 
+
+// background color changing
+let intervalId;
+function startChange() {
+    if (!intervalId) {
+        intervalId = setInterval(changeColor, 1000);
+    }
+}
+
+function changeColor() {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = `#${randomColor}`
+	
+    
+}
+
+
+document.getElementById('joke-btn').addEventListener('click', startChange)
+
 jokeBtn.addEventListener('click', generateJoke);
 document.addEventListener('DOMContentLoaded', generateJoke)
